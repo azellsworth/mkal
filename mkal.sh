75d7344fa38c488bfbaa3a937b78bff29e2d0369
naomiajacobs <naomiajacobs@gmail.com>
2016-02-23 11:01:55 -0800

    Insert alias into profile of shell being used; add postinstall.sh and preinstall.sh

--- mkal.sh
+++ mkal.sh
@@ -1,7 +1,19 @@
-  #!/bin/bash
+#!/bin/bash
 
 X=$(pwd)   #saves present working directory to X
-WD=${X// /\\ }  #replaces spaces with '\ '
-  sed -i '' -e '$a\' ~/.bash_profile   #creates a new line at the end of bash_profile if necessary
-  echo 'alias' "$1"'="cd ' $WD'"' >>~/.bash_profile   #creates an alias for pwd at end of bash_profile
-  . ~/.bash_profile   #reloads bash_profile
+
+if [ -n "$ZSH_VERSION" ]; then
+   # assume Zsh
+   WD=${X// /\\ }  #replaces spaces with '\ '
+     sed -i '' -e '$a\' ~/.bash_profile   #creates a new line at the end of bash_profile if necessary
+     echo 'alias' "$1"'="cd ' $WD'"' >>~/.zshrc   #creates an alias for pwd at end of zsh profile
+     source ~/.zshrc   #reloads .zshrc
+fi
+
+if [ -n "$BASH_VERSION" ]; then
+   # assume Bash
+   WD=${X// /\\ }  #replaces spaces with '\ '
+     sed -i '' -e '$a\' ~/.bash_profile   #creates a new line at the end of bash_profile if necessary
+     echo 'alias' "$1"'="cd ' $WD'"' >>~/.bash_profile   #creates an alias for pwd at end of bash_profile
+     source ~/.bash_profile   #reloads bash_profile 
+fi
